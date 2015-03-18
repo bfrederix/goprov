9e185f5cf44534ba08a10b5fc6d9918f214a4913
Freddy Fredericks <bfrede200@coccpl-6twfd57.local>
2015-03-18 14:40:11 -0600

    Added all the base pages for the admin dropdown.

--- src/voteprov/main.go
+++ src/voteprov/main.go
@@ -24,6 +24,15 @@ func CreateHandler() *mux.Router {
 	// Admin Pages
 	a := r.PathPrefix("/admin").Subrouter()
 	a.HandleFunc("/pre_show/", InstructionPage)
+	a.HandleFunc("/create_show/", CreateShowPage)
+	a.HandleFunc("/vote_types/", VoteTypesPage)
+	a.HandleFunc("/suggestion_pools/", SuggestionPoolPage)
+	a.HandleFunc("/create_medals/", CreateMedalsPage)
+	a.HandleFunc("/add_players/", AddPlayersPage)
+	a.HandleFunc("/delete_tools/", DeleteToolsPage)
+	a.HandleFunc("/js_test/", JSTestPage)
+	a.HandleFunc("/export_emails/", ExportEmailsPage)
+
 
 	// Facebook Callback
 	h.HandleFunc("/fb_login/", FacebookLogin)
--- src/voteprov/pages.go
+++ src/voteprov/pages.go
@@ -46,3 +46,123 @@ func InstructionPage(rw http.ResponseWriter, r *http.Request) {
         http.Error(rw, err.Error(), http.StatusInternalServerError)
     }
 }
+
+
+func CreateShowPage(rw http.ResponseWriter, r *http.Request) {
+	dc := DefaultContext{}
+	AuthContext(rw, r, &dc)
+	// Redirect if not admin
+	if !dc.IsAdmin {
+		AdminRedirect(rw, r)
+	}
+	dc.Page = "create_show"
+    err := templates.ExecuteTemplate(rw, "create_show", &dc)
+	if err != nil {
+        http.Error(rw, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+
+func VoteTypesPage(rw http.ResponseWriter, r *http.Request) {
+	dc := DefaultContext{}
+	AuthContext(rw, r, &dc)
+	// Redirect if not admin
+	if !dc.IsAdmin {
+		AdminRedirect(rw, r)
+	}
+	dc.Page = "vote_types"
+    err := templates.ExecuteTemplate(rw, "vote_types", &dc)
+	if err != nil {
+        http.Error(rw, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+
+func SuggestionPoolPage(rw http.ResponseWriter, r *http.Request) {
+	dc := DefaultContext{}
+	AuthContext(rw, r, &dc)
+	// Redirect if not admin
+	if !dc.IsAdmin {
+		AdminRedirect(rw, r)
+	}
+	dc.Page = "suggestion_pools"
+    err := templates.ExecuteTemplate(rw, "suggestion_pools", &dc)
+	if err != nil {
+        http.Error(rw, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+
+func CreateMedalsPage(rw http.ResponseWriter, r *http.Request) {
+	dc := DefaultContext{}
+	AuthContext(rw, r, &dc)
+	// Redirect if not admin
+	if !dc.IsAdmin {
+		AdminRedirect(rw, r)
+	}
+	dc.Page = "create_medals"
+    err := templates.ExecuteTemplate(rw, "create_medals", &dc)
+	if err != nil {
+        http.Error(rw, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+
+func AddPlayersPage(rw http.ResponseWriter, r *http.Request) {
+	dc := DefaultContext{}
+	AuthContext(rw, r, &dc)
+	// Redirect if not admin
+	if !dc.IsAdmin {
+		AdminRedirect(rw, r)
+	}
+	dc.Page = "add_players"
+    err := templates.ExecuteTemplate(rw, "add_players", &dc)
+	if err != nil {
+        http.Error(rw, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+
+func DeleteToolsPage(rw http.ResponseWriter, r *http.Request) {
+	dc := DefaultContext{}
+	AuthContext(rw, r, &dc)
+	// Redirect if not admin
+	if !dc.IsAdmin {
+		AdminRedirect(rw, r)
+	}
+	dc.Page = "delete_tools"
+    err := templates.ExecuteTemplate(rw, "delete_tools", &dc)
+	if err != nil {
+        http.Error(rw, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+
+func JSTestPage(rw http.ResponseWriter, r *http.Request) {
+	dc := DefaultContext{}
+	AuthContext(rw, r, &dc)
+	// Redirect if not admin
+	if !dc.IsAdmin {
+		AdminRedirect(rw, r)
+	}
+	dc.Page = "js_test"
+    err := templates.ExecuteTemplate(rw, "js_test", &dc)
+	if err != nil {
+        http.Error(rw, err.Error(), http.StatusInternalServerError)
+    }
+}
+
+
+func ExportEmailsPage(rw http.ResponseWriter, r *http.Request) {
+	dc := DefaultContext{}
+	AuthContext(rw, r, &dc)
+	// Redirect if not admin
+	if !dc.IsAdmin {
+		AdminRedirect(rw, r)
+	}
+	dc.Page = "export_emails"
+    err := templates.ExecuteTemplate(rw, "export_emails", &dc)
+	if err != nil {
+        http.Error(rw, err.Error(), http.StatusInternalServerError)
+    }
+}
